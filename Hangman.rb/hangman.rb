d7dceec77cc81ef61cf31e8569b52e6b3d355ecb
Justin McCoy <31171668+Just-A-Engineer@users.noreply.github.com>
2022-02-01 17:09:27 -0700

    Deleted Hangman.rb Directory and Files

--- Hangman.rb/hangman.rb
+++
@@ -1,7 +0,0 @@
-def select_word
-  word_file = "words.txt"
-  word_list = File.open(word_file)
-  words = word_list.readlines
-  index = rand(words.length - 1)
-  mystery_word = words[index].chomp
-end
