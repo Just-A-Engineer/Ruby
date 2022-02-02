f747aad65c10ec4fdc8d8ae49c62c57c4197f3b8
Justin McCoy <31171668+Just-A-Engineer@users.noreply.github.com>
2022-02-02 13:42:58 -0700

    Updated hangman.rb file
    Updated hangman.rb file with comments and removed header comments

--- Hangman.rb
+++ Hangman.rb
@@ -1,22 +1,15 @@
-# This is a basic program on Ruby
-#
-# Try to modify and run it and check out
-# the output in the terminal below
-#
-# Happy coding! :-)
-
+#Words Array (will be working in a text file instead of an array)
 $words = Array["ruby", "rails", "python", "cat", "array", "dog", "burn", "chicken", "cow"]
-
+#This method creates two variables mystery_word and mystery_letters
 def select_word()
   $mystery_word = $words.shuffle.first
-
   $mystery_letters = $mystery_word.split("")
 end
-
+#This method defines what difficulty the player would like
 def diff()
   puts "What difficulty would you like to play with? (Easy, Medium, or Hard)"
   difficulty = gets.chomp
-
+  #If the difficulty entered is equal to any of the difficulty choices
   if difficulty == "easy" or difficulty == "Easy"
     puts "You get 5 lives."
     return 5
@@ -31,26 +24,26 @@ def diff()
     diff()
   end
 end
-
+#This is the initial gameplay method it contains most of the gameplay elements
 def gameplay()
   puts "--------------------"
   puts "Welcome to the game!"
   puts "--------------------"
-
+  #Sets lives = what the diff(difficulty) method returns
   lives = diff()
   select_word()
-  
+  #While loop checking if lives have depleted to 0 or not
   while lives != 0
     puts "What is your guess?"
-
+    #This takes a guess from the player 
     guess = gets.chomp
-    
+    #This checks if the guess is equal to the mystery_word
     if $mystery_word == guess
       puts "That is the word!"
       puts "-----------------"
       break 
     end
-    
+    #This checks to see if the guess is in the array of letters created in mystery_letters variable
     if $mystery_letters.include? guess
       puts "That letter is in the word!"
     else
@@ -61,12 +54,12 @@ def gameplay()
       puts "You have #{lives.to_s} lives left!"
     end
   end
-
+  #This if statement checks if they have depleted their lives after the while loop
   if lives == 0
     puts "You lost. Feel free to play again."
   else
     puts "You won!"
   end
 end
-
+#Calling initial gameplay() method. 
 gameplay()
