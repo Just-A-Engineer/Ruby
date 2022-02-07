38794e236ecdaa948594d3065e3c76c78b3e855a
Justin McCoy <31171668+Just-A-Engineer@users.noreply.github.com>
2022-02-07 10:15:41 -0700

    Removed unnecessary space between statements

--- WebTesting/features/step_definitions/test_step.rb
+++ WebTesting/features/step_definitions/test_step.rb
@@ -10,11 +10,9 @@ end
 Then('I should see {string}') do |string|
     page.has_css?(".title__hero page__title__hero", text: string)
 end
-
 Then('I should see the header {string}') do |string|
     page.has_css?(".title__block", text: string)
 end
-
 Then('I should also see {string}') do |string|
     page.has_css?(".nextaction__top__headline", text: string)
 end
