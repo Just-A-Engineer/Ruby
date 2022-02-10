444633591f0deca8189df41e29b36cc0642df90d
Justin McCoy <31171668+Just-A-Engineer@users.noreply.github.com>
2022-02-09 22:56:39 -0700

    Adjusted user_input to be an integer
    Adjusted user_input to be an integer with method .to_i. Removes bug when you put in lower numbers it says the wrong generation. However, pulled a new bug with JSON::ParserError. Working out the kinks of it tonight or tomorrow.

--- Pokedex/Pokedex.rb
+++ Pokedex/Pokedex.rb
@@ -1,6 +1,6 @@
 require "poke-api-v2"
 #Grabs pokemon from PokeApi using the .get method
-def get_pokemon(pokemon)   
+def get_pokemon(pokemon)
     $mon = PokeApi.get(pokemon: pokemon)
 end
 #Run method contains most of the code that runs the Pokedex
@@ -8,12 +8,12 @@ def run
     #Welcomes User and asks them to choose a pokemon number
     puts "Welcome to the Pokedex!"
     puts "------------------------"
-    puts "Please enter the number of the Pokemon you would like to reach?"
+    puts "Please enter the number/ID of the Pokemon you'd like info on: "
     puts "------------------------"
     #Gets user input to see which pokemon they would like information on
-    user_input = gets.chomp
+    user_input = (gets.chomp).to_i
     #Until user decides to exit the Pokedex, it keeps running this function
-    until user_input == "exit"
+    while user_input != "exit"
         #Uses get_pokemon() method to grab the pokemon the user chooses
         poke = get_pokemon(user_input)
         #Pokemon Name, Weight, Height, and Generation
@@ -21,21 +21,21 @@ def run
         mon_weight = ($mon.weight * 0.220462).to_i #(Hectograms to Pounds)
         mon_height = ($mon.height * 3.93701).to_i #(Decimeters to Inches)
         #Hard coded way to get generation of pokemon
-        if user_input <= 151.to_s and user_input > 0.to_s
+        if user_input > 0 and user_input <= 151
             mon_gen = "Generation I"
-        elsif user_input > 151.to_s and user_input <= 251.to_s
+        elsif user_input > 151 and user_input <= 251
             mon_gen = "Generation II"
-        elsif user_input > 251.to_s and user_input <= 386.to_s
+        elsif user_input > 251 and user_input <= 386
             mon_gen = "Generation III"
-        elsif user_input > 386.to_s and user_input <= 493.to_s
+        elsif user_input > 386 and user_input <= 493
             mon_gen = "Generation IV"
-        elsif user_input > 493.to_s and user_input <= 649.to_s
+        elsif user_input > 493 and user_input <= 649
             mon_gen = "Generation V"
-        elsif user_input > 649.to_s and user_input <= 721.to_s
+        elsif user_input > 649 and user_input <= 721
             mon_gen = "Generation VI"
-        elsif user_input > 721.to_s and user_input <= 809.to_s
+        elsif user_input > 721 and user_input <= 809
             mon_gen = "Generation VII"
-        elsif user_input > 809.to_s and user_input <= 898.to_s
+        elsif user_input > 809 and user_input <= 898
             mon_gen = "Generation VIII"
         end
         #Prints out information on Pokemon chosen
@@ -50,7 +50,7 @@ def run
         puts "------------------------"
         puts "Put in another number (Or type exit to leave the app):"
         puts "------------------------"
-        user_input = gets.chomp
+        user_input = (gets.chomp).to_i
     end
 end
 #Runs the run method/function 
